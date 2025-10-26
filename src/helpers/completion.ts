1681edd9036acb8087395e2a0b3a0b25362ef5c6
837471654@qq.com <837471654@qq.com>
2025-10-26 19:44:49 +0800

    1. 忽略产物 2.添加正则表达式匹配

--- src/helpers/completion.ts
+++ src/helpers/completion.ts
@@ -4,11 +4,19 @@ import {
   ChatCompletionRequestMessage,
   Model,
 } from 'openai';
-
 import dedent from 'dedent';
 import { IncomingMessage } from 'http';
+import { KnownError } from './error';
+import { streamToIterable } from './stream-to-iterable';
+// import { detectShell } from './os-detect';
 import type { AxiosError } from 'axios';
+import { streamToString } from './stream-to-string';
+import './replace-all-polyfill';
+// import i18n from './i18n';
+import { stripRegexPatterns } from './strip-regex-patterns';
 import readline from 'readline';
+
+const explainInSecondRequest = true;
 
 function getOpenAi(key: string, apiEndpoint: string) {
   const openAi = new OpenAIApi(
@@ -17,7 +25,8 @@ function getOpenAi(key: string, apiEndpoint: string) {
   return openAi;
 }
 
-// AI的输出代码的格式大概率像github风格的代码块：```bash  使用正则表达式来匹配这个歌格式
+// Openai outputs markdown format for code blocks. It oftne uses
+// a github style like: "```bash"
 const shellCodeExclusions = [/```[a-zA-Z]*\n/gi, /```[a-zA-Z]*/gi, '\n'];
 
 export async function getScriptAndInfo({
@@ -63,7 +72,7 @@ export async function generateCompletion({
   try {
     const completion = await openAi.createChatCompletion(
       {
-        model: model || 'gpt-4o-mini',  // 默认模型 gpt-4o-mini
+        model: model || 'gpt-4o-mini',
         messages: Array.isArray(prompt)
           ? prompt
           : [{ role: 'user', content: prompt }],
@@ -102,7 +111,12 @@ export async function generateCompletion({
     if (response?.status === 429) {
       throw new KnownError(
         dedent`
-        Request to AI failed with status 429. Full message from OpenAI:` +
+        Request to OpenAI failed with status 429. This is due to incorrect billing setup or excessive quota usage. Please follow this guide to fix it: https://help.openai.com/en/articles/6891831-error-code-429-you-exceeded-your-current-quota-please-check-your-plan-and-billing-details
+
+        You can activate billing here: https://platform.openai.com/account/billing/overview . Make sure to add a payment method if not under an active grant from OpenAI.
+
+        Full message from OpenAI:
+      ` +
           '\n\n' +
           messageString +
           '\n'
@@ -110,7 +124,7 @@ export async function generateCompletion({
     } else if (response && message) {
       throw new KnownError(
         dedent`
-        Request to AI failed with status ${response?.status}:
+        Request to OpenAI failed with status ${response?.status}:
       ` +
           '\n\n' +
           messageString +
@@ -143,6 +157,33 @@ export async function getExplanation({
   });
   const iterableStream = streamToIterable(stream);
   return { readExplanation: readData(iterableStream) };
+}
+
+export async function getRevision({
+  prompt,
+  code,
+  key,
+  model,
+  apiEndpoint,
+}: {
+  prompt: string;
+  code: string;
+  key: string;
+  model?: string;
+  apiEndpoint: string;
+}) {
+  const fullPrompt = getRevisionPrompt(prompt, code);
+  const stream = await generateCompletion({
+    prompt: fullPrompt,
+    key,
+    number: 1,
+    model,
+    apiEndpoint,
+  });
+  const iterableStream = streamToIterable(stream);
+  return {
+    readScript: readData(iterableStream, ...shellCodeExclusions),
+  };
 }
 
 export const readData =
@@ -220,3 +261,70 @@ export const readData =
 
       resolve(data);
     });
+
+function getExplanationPrompt(script: string) {
+  return dedent`
+    ${explainScript} Please reply in ${i18n.getCurrentLanguagenName()}
+
+    The script: ${script}
+  `;
+}
+
+function getShellDetails() {
+  const shellDetails = detectShell();
+
+  return dedent`
+      The target shell is ${shellDetails}
+  `;
+}
+const shellDetails = getShellDetails();
+
+const explainScript = dedent`
+  Please provide a clear, concise description of the script, using minimal words. Outline the steps in a list format.
+`;
+
+function getOperationSystemDetails() {
+  const os = require('@nexssp/os/legacy');
+  return os.name();
+}
+const generationDetails = dedent`
+    Only reply with the single line command surrounded by three backticks. It must be able to be directly run in the target shell. Do not include any other text.
+
+    Make sure the command runs on ${getOperationSystemDetails()} operating system.
+  `;
+
+function getFullPrompt(prompt: string) {
+  return dedent`
+    Create a single line command that one can enter in a terminal and run, based on what is specified in the prompt.
+
+    ${shellDetails}
+
+    ${generationDetails}
+
+    ${explainInSecondRequest ? '' : explainScript}
+
+    The prompt is: ${prompt}
+  `;
+}
+
+function getRevisionPrompt(prompt: string, code: string) {
+  return dedent`
+    Update the following script based on what is asked in the following prompt.
+
+    The script: ${code}
+
+    The prompt: ${prompt}
+
+    ${generationDetails}
+  `;
+}
+
+export async function getModels(
+  key: string,
+  apiEndpoint: string
+): Promise<Model[]> {
+  const openAi = getOpenAi(key, apiEndpoint);
+  const response = await openAi.listModels();
+
+  return response.data.data.filter((model) => model.object === 'model');
+}
